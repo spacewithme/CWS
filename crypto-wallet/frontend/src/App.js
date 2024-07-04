6ca24081772f48c1ea250342eeb2159009eed86c
[Rohit Shrivastava] <[rohitshrivastava610@gmail.com]>
2024-07-04 20:56:46 +0530

    app.js

--- crypto-wallet/frontend/src/App.js
+++ crypto-wallet/frontend/src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Register from './components/Register';
+import Login from './components/Login';
+import Dashboard from './components/Dashboard';
+import Wallet from './components/Wallet';
+import Balance from './components/Balance';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    return (
+        <Router>
+            <Navbar />
+            <Switch>
+                <Route path="/" exact component={Dashboard} />
+                <Route path="/register" component={Register} />
+                <Route path="/login" component={Login} />
+                <Route path="/dashboard" component={Dashboard} />
+                <Route path="/wallet" component={Wallet} />
+                <Route path="/balance" component={Balance} />
+            </Switch>
+        </Router>
+    );
+};
 
 export default App;
