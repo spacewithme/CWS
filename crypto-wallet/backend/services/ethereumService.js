ae7d2989d00990e7016f3e15d20b5a5a3e418f8e
Rohit Shrivastava <rohitshrivastava610@gmail.com>
2024-07-05 10:12:06 +0530

    update

--- crypto-wallet/backend/services/ethereumService.js
+++ crypto-wallet/backend/services/ethereumService.js
@@ -1,7 +1,7 @@
 const Web3 = require('web3');
 
 // Replace 'YOUR_INFURA_PROJECT_ID' with your actual Infura project ID
-const web3 = new Web3('https://mainnet.infura.io/v3/8a9b30ac388647138789057e70c12652');
+const web3 = new Web3(new Web3.providers.HttpProvider('https://mainnet.infura.io/v3/8a9b30ac388647138789057e70c12652'));
 
 const createEthereumWallet = () => {
     const account = web3.eth.accounts.create();
